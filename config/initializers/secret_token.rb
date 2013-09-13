25fd444aac1c618bc7a40c3c6c6452b96ae631b2
Filipe Guth <lfguth@gmail.com>
2013-09-13 00:08:45 -0300

    rails. to support sass without manually compiling

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = 'bae5aeefd2a4b00778ad271c2251708c8fd74e2f51047de8cebb97cd7f0a5ee89f0ba67bebc4d19db506d69569e0b1987e2f106ea60d9e0b7d72dc4ac009e98b'
